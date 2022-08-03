1a571b720c17e5b270b55ff02d129445ca2a5a69
avlokha <avlokhashvili93@gmail.com>
2022-08-03 11:09:44 +0400

    add html , css ,js

---
+++ app.js
@@ -0,0 +1,13 @@
+const squar = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+
+for (let i = 0; i < squar.length; i++) {
+  for (let j = 0; j < squar[i].length; j++) {
+    console.log(`[${i}][${j}] = ${squar[i][j]}`);
+  }
+}
